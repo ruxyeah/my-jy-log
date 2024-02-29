d44e3616b8c57aa8c2682065e029796b79336734
ruxyeah <ruxyeah@163.com>
2024-02-29 15:09:44 +0800

    dest

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  dest: "./dev-ops/nginx/html",
+  dest: "public",
   base: "/",
 
   lang: "zh-CN",
